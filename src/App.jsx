197b33fbf8e4eca2673649e38b88d1ab59dd1871
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2024-10-19 19:17:27 +0200

    the exercise finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
 import './App.css'
+import {useState} from 'react';
+import ReactDom  from 'react-dom/client'
+
+
+/*El primer ejercicio*/
+/*function App() {
+ const [name, setName] = useState('Sofia')
+ const [newName, setNewName] = useState('')
+ const teachers = ['Data', 'Reyes', 'Yolanda'];
+ return (
+    <div>
+    <h2>Teacher Name: {name}</h2>
+    <ul>
+    {teachers.map((teacher) => (
+          <li key={teacher} onClick={() => setName(teacher)}>
+            {teacher}</li>))}
+    </ul>
+    </div>
+ )
+}*/
+
+/*BONUS + el primer ejercicio*/
 
 function App() {
- //Aquí tu código
-}
+    const [name, setName] = useState('Sofia')
+    const [newName, setNewName] = useState('')
+    const teachers = ['Data', 'Reyes', 'Yolanda'];
+    const changeName = (event) => {
+        event.preventDefault();
+        if(newName !== '') {
+            setName(newName)
+            setNewName('')
+        }
+    }
+    return (
+        <div>
+            <h2>Teacher Name: {name}</h2>
+        <form onSubmit={changeName}>
+            <input type="text" value={newName} onChange={(e) => setNewName(e.target.value)} placeholder="add a name"></input>
+            <button type="submit">Add</button>
+        </form>
+        <ul>
+            {teachers.map((teacher) => (
+            <li key={teacher} onClick={() => setName(teacher)}>
+            {teacher}</li>))}
+        </ul>
+        </div>
+   )}
 
 export default App
